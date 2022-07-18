9d697f093da9f1a4074cbed1a0ef9a3fa57b7af4
muzzammil194 <muzzammil194@gmail.com>
2022-07-18 21:46:48 +0500

    practics video

--- src/index.js
+++ src/index.js
@@ -1,17 +1,134 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const name="John";
+const lname="bravo";
+let date=new Date().getTime();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const inlineStyle={
+  fontSize: '7em',
+  margin: '0.67em 0',
+  fontStyle: 'italic',
+  color: 'blueviolet',
+}
+root.render(
+  <>
+    <h1 style={inlineStyle}>Hello, world!</h1>
+<p>boys {3*3}</p>
+<p>boys {Math.random()}</p>
+<h2>Makes some {` ${name} ${lname}`}</h2>
+<h3>Current Date is {` ${date}`}</h3>
+<h4>Current Time is {`${date}`}</h4>
+<h4 contentEditable="true">Current Time is {`${date}`}</h4>
+<h4 className='h1'>Current Time is {`${date}`}</h4>
+ <img src='https://unsplash.com/photos/7KLa-xLbSXA' alt=''/>
+  </>
+);
+// // quiz 2 video 12
+// const name="John";
+// const lname="bravo";
+// let date=new Date().getTime();
+// ReactDOM.render(
+// <>
+// <h1>Hello, world!</h1>
+// <p>boys {3*3}</p>
+// <p>boys {Math.random()}</p>
+// <h2>Makes some {` ${name} ${lname}`}</h2>
+// <h3>Current Date is {` ${date}`}</h3>
+// <h4>Current Time is {`${date}`}</h4>
+// <h4 contentEditable="true">Current Time is {`${date}`}</h4>
+// <h4 className='h1'>Current Time is {`${date}`}</h4>
+// <img src='https://unsplash.com/photos/7KLa-xLbSXA' alt=''/>
+// </>
+// , document.getElementById("root"));
+
+// // template litrals is just like a string ${name} ${lname}
+// const name="John";
+// const lname="bravo";
+// ReactDOM.render(
+// <>
+// <h1>Hello, world!</h1>
+// <p>boys {3*3}</p>
+// <p>boys {Math.random()}</p>
+// <h2>Makes some {` ${name} ${lname}`}</h2>
+// </>
+// , document.getElementById('root'));
+
+
+// // this is expression syntax {variable} or {arthmatical expression}
+// const name="John";
+// ReactDOM.render(
+// <>
+// <h1>Hello, world!</h1>
+// <p>boys {3*3}</p>
+// <p>boys {Math.random()}</p>
+// <h2>Makes some {name}</h2>
+// </>
+// , document.getElementById('root'));
+
+
+
+
+
+// ReactDOM.render(
+// <>
+// <h1>Hello, world!</h1>
+// <p>boys</p>
+// <h2>Makes some noise</h2>
+// <ol>
+//   <li>Coffee</li>
+//   <li>Tea</li>
+//   <li>Milk</li>
+// </ol>
+// </>
+// , document.getElementById('root'));
+
+
+// // React.Fragment is sugar form < >  </>
+// ReactDOM.render(
+// <React.Fragment>
+// <h1>Hello, world!</h1>
+// <p>boys</p>
+// <h2>Makes some noise</h2>
+// </React.Fragment>
+// , document.getElementById('root'));
+// ReactDOM.render(
+// <React.Fragment>
+// <h1>Hello, world!</h1>
+// <p>boys</p>
+// <h2>Makes some noise</h2>
+// </React.Fragment>
+// , document.getElementById('root'));
+
+
+
+// ReactDOM.render(
+// [
+// <h1>Hello, world!</h1>, 
+// <p>This is a paragraph.</p>
+// ]
+// , document.getElementById('root'));
+// ReactDOM.render(
+// <div>
+// <h1>Hello, world!</h1>
+// <p>This is a paragraph.</p>
+// </div>
+// , document.getElementById('root'));
+
+
+ 
+
+
+//WRITE IN BEBEL CONVERTION 
+// ReactDOM.render("h1",document.getElementById('root'),"Muzzammil Shaikh", );
+
+
+// let h1 =document.getElementById("h1");
+// h1.innerHTML="Muzzammil Shaikh";
+// h1.style.color="red";
+// document.getElementById("root").appendChild(h1);
+
